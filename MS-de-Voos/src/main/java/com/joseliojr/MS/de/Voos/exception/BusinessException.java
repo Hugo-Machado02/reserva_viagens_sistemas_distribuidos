af9b11556fbe65eb8b091304b17c824c5a1d08c4
Joselio Jr. <joselio.fc@hotmail.com>
2025-12-07 01:25:08 -0300

    ✨ feat: Implementação dos Exceptions

---
+++ MS-de-Voos/src/main/java/com/joseliojr/MS/de/Voos/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.joseliojr.MS.de.Voos.exception;
+
+public class BusinessException extends RuntimeException {
+    
+    public BusinessException(String message) {
+        super(message);
+    }
+}
---
+++ MS-de-Voos/src/main/java/com/joseliojr/MS/de/Voos/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.joseliojr.MS.de.Voos.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
