68c29ec4b8c7c3eee31ad8912f04a30f1c2169d4
Hugo Machado <hugo_machado02@hotmail.com>
2025-12-07 20:57:57 -0300

    ♻️ refactor: Refatora o Application

--- src/ms_hoteis/src/main/java/br/com/hugomachadodev/ms_hoteis/MsHoteisApplication.java
+++ src/ms_hoteis/src/main/java/br/com/hugomachadodev/ms_hoteis/MsHoteisApplication.java
@@ -2,12 +2,13 @@ package br.com.hugomachadodev.ms_hoteis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsHoteisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsHoteisApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MsHoteisApplication.class, args);
+    }
 }
