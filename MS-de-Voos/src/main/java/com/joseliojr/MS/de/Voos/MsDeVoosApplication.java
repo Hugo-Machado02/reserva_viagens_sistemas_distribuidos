b83364c7cc0799256acf113bc1697d1e10ddbbe8
Joselio Jr. <joselio.fc@hotmail.com>
2025-12-07 01:21:57 -0300

    ✨ feat: Configuração do projeto
    Criação e atualização das configurações necessárias para execução do projeto

---
+++ MS-de-Voos/src/main/java/com/joseliojr/MS/de/Voos/MsDeVoosApplication.java
@@ -0,0 +1,13 @@
+package com.joseliojr.MS.de.Voos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDeVoosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDeVoosApplication.class, args);
+	}
+
+}
---
+++ MS-de-Voos/src/test/java/com/joseliojr/MS/de/Voos/MsDeVoosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseliojr.MS.de.Voos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDeVoosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
