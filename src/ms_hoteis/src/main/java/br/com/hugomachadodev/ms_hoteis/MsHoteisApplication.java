da7ade3a9e2b5b22527382d6ccbd23e0f34986eb
Hugo Machado <hugo_machado02@hotmail.com>
2025-12-07 00:09:10 -0300

    ✨ feat: Adiciona base para o Microsserviço de Hoteis

---
+++ src/ms_hoteis/src/main/java/br/com/hugomachadodev/ms_hoteis/MsHoteisApplication.java
@@ -0,0 +1,13 @@
+package br.com.hugomachadodev.ms_hoteis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHoteisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHoteisApplication.class, args);
+	}
+
+}
---
+++ src/ms_hoteis/src/test/java/br/com/hugomachadodev/ms_hoteis/MsHoteisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hugomachadodev.ms_hoteis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHoteisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
