ddc610fc74f0c2a3f99dbe9714bd947a1a0f8caf
Joselio Jr. <joselio.fc@hotmail.com>
2025-12-08 18:40:32 -0300

    🗑️ remove: Removendo arquivo desnecessários

--- src/ms_hoteis/src/main/java/br/com/hugomachadodev/ms_hoteis/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hugomachadodev.ms_hoteis.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
