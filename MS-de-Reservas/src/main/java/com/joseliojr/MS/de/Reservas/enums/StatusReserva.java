a1138cbf104bbddfdaba75dc58e46f13826b087f
Hugo Machado <hugo_machado02@hotmail.com>
2025-12-08 01:18:43 -0300

    ✨ feat: Base para o serviço do Eureka

--- MS-de-Reservas/src/main/java/com/joseliojr/MS/de/Reservas/enums/StatusReserva.java
+++
@@ -1,7 +0,0 @@
-package com.joseliojr.MS.de.Reservas.enums;
-
-public enum StatusReserva {
-    PENDENTE,
-    CONFIRMADA,
-    CANCELADA
-}
--- MS-de-Voos/src/main/java/com/joseliojr/MS/de/Voos/exception/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package com.joseliojr.MS.de.Voos.exception;
-
-public class BusinessException extends RuntimeException {
-    
-    public BusinessException(String message) {
-        super(message);
-    }
-}
